4238175714199ca73dd591820f00d6032a04e266
Alan Seto <alantseto@gmail.com>
2017-04-01 12:36:30 -0500

    Initial release

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# gas-rate
+# gasoline
+
+Simple CLI utility for U.S. state gas prices updated from [AAA's Gas Prices](http://gasprices.aaa.com/).
+
+![Gasoline Screenshot](screenshot.png)
+
+## Install
+
+Gasoline is not published in the npm Registry so installation is done within the project directory with:
+
+```sh
+$ npm install --global
+```
+
+## Usage
+
+State abbreviations are the 50 U.S. states, including `DC` for Washington D.C. and `US` for national U.S. gas prices.
+
+```sh
+$ gasoline -s [state abbreviation]
+```
 
 ## License
 
--- gasoline.js
+++ gasoline.js
@@ -2,27 +2,63 @@
 
 const program = require('commander');
 const request = require('request');
+const chalk = require('chalk');
+const log = console.log;
+
+const states = [
+  'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+  'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+  'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+  'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+  'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY',
+  'DC', 'US'
+];
 
 program
-  .version('0.0.1')
+  .version('0.1.0')
   .option('-s, --state [state]', 'state to search gas prices for')
   .parse(process.argv);
 
 if (!program.state) {
-  console.log('Please enter a State');
+  log(chalk.red('Please enter a state abbreviation'));
+  return;
+} else if (!states.includes(program.state.toUpperCase())) {
+  log(chalk.red(`State abbreviation not found`));
   return;
 }
 
-request(`http://gasprices.aaa.com/?state=${program.state}`, (error, response, body) => {
-  const index = body.search('Current Avg.');
-  const regular = body.substring(index + 43, index + 48);
-  const midGrade = body.substring(index + 79, index + 84);
-  const premium = body.substring(index + 115, index + 120);
-  const diesel = body.substring(index + 151, index + 156);
+request(`http://gasprices.aaa.com/?state=${program.state.toUpperCase()}`, (error, response, body) => {
+  const updateIndex = body.search('Prices updated as of');
+  const endUpdateIndex = body.search('m ET');
+  const dateUpdated = body.substring(updateIndex + 28, endUpdateIndex + 4);
+  log(chalk.magenta(`Average gas prices for ${program.state} as of ${dateUpdated}`));
+  log(chalk.cyan.underline('                Regular Mid-Grade   Premium    Diesel'));
 
-  console.log(`Average gas prices for: ${program.state}`);
-  console.log(`Regular: $${regular}`);
-  console.log(`Mid-Grade: $${midGrade}`);
-  console.log(`Premium: $${premium}`);
-  console.log(`Diesel: $${diesel}`);
+  let index = body.search('Current Avg.');
+  let regular = body.substring(index + 43, index + 48);
+  let midGrade = body.substring(index + 79, index + 84);
+  let premium = body.substring(index + 115, index + 120);
+  let diesel = body.substring(index + 151, index + 156);
+  log(chalk.red(`Current Avg:     $${regular}    $${midGrade}    $${premium}    $${diesel}`));
+
+  index = body.search('Yesterday Avg.');
+  regular = body.substring(index + 45, index + 50);
+  midGrade = body.substring(index + 81, index + 86);
+  premium = body.substring(index + 117, index + 122);
+  diesel = body.substring(index + 153, index + 158);
+  log(chalk.green(`Yesterday Avg:   $${regular}    $${midGrade}    $${premium}    $${diesel}`));
+
+  index = body.search('Week Ago Avg.');
+  regular = body.substring(index + 44, index + 49);
+  midGrade = body.substring(index + 80, index + 85);
+  premium = body.substring(index + 116, index + 121);
+  diesel = body.substring(index + 152, index + 157);
+  log(chalk.yellow(`Week Ago Avg:    $${regular}    $${midGrade}    $${premium}    $${diesel}`));
+
+  index = body.search('Month Ago Avg.');
+  regular = body.substring(index + 45, index + 50);
+  midGrade = body.substring(index + 81, index + 86);
+  premium = body.substring(index + 117, index + 122);
+  diesel = body.substring(index + 153, index + 158);
+  log(chalk.blue(`Month Ago Avg:   $${regular}    $${midGrade}    $${premium}    $${diesel}`));
 });
